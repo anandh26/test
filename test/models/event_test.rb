e201a58d57bc99d001ee9858cefd5163906d2b9e
MalarCB <malarvizhy.soupramanien@careerbuilder.com>
2016-04-12 10:32:03 +0200

    add more test

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -26,4 +26,16 @@ class EventTest < ActiveSupport::TestCase
     assert_equal Date.new(2014, 8, 10), availabilities[6][:date]
     assert_equal 7, availabilities.length
   end
+
+  test 'when all the appointments booked' do
+    Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
+    Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-11 09:30"), ends_at: DateTime.parse("2014-08-11 12:30")
+
+    availabilities = Event.availabilities DateTime.parse("2014-08-10")
+    assert_equal Date.new(2014, 8, 10), availabilities[0][:date]
+    assert_equal [], availabilities[0][:slots]
+    assert_equal [], availabilities[1][:slots]
+    assert_equal Date.new(2014, 8, 16), availabilities[6][:date]
+    assert_equal 7, availabilities.length
+  end
 end
